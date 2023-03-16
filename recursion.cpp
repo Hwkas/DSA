11564b33b028b6db40b145601e26d2eb70f3b133
Amaan Saifi <amaansaifi999@gmail.com>
2023-03-16 23:44:39 +0530

    recursion update

--- recursion.cpp
+++ recursion.cpp
@@ -15,12 +15,53 @@ inline int factorial(int n){
 // }
 
 
+// inline recursive function
+inline int fibonacci(int n){
+    return (n==0 || n==1) ? n : fibonacci(n-1)+fibonacci(n-2);
+}
+
+// normal recursive function
+// int fibonacci(int n){
+//     if(n==0 || n==1){
+//         return n;
+//     }
+//     return fibonacci(n-1)+fibonacci(n-2);
+// }
+
+
+void get_array_values(int arr[], int n){
+
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+}
+
+void print_array(int arr[], int n){
+
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
 int main(){
 
-    cout<<"Enter the number to find it's factorial: ";
+    // cout<<"Enter the number to find it's factorial & fibonacci: ";
+    // int n;
+    // cin>>n;
+    
+    // cout<<"The factorial of "<<n<<" is: "<<factorial(n)<<endl;
+    
+    // cout<<"The fibonacci of "<<n<<" is: "<<fibonacci(n)<<endl;
+
+    cout<<"Enter the number of elements in array: ";
     int n;
     cin>>n;
-    
-    cout<<"The factorial of n is: "<<factorial(n)<<endl;
 
+    int* arr = new int [n];
+
+    get_array_values(arr, n);
+
+    print_array(arr, n);
+
+    delete [] arr;
 }
