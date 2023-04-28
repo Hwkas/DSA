88dab6277d75324358d1163b2c4cc619f9469a62
Amaan Saifi <amaansaifi999@gmail.com>
2023-04-28 14:45:23 +0530

    singly linked list completed

--- linked_list/linked_list.cpp
+++ linked_list/linked_list.cpp
@@ -13,6 +13,21 @@ public:
         this->data = data;
         this->next = NULL;
     }
+
+    ~Node()
+    {
+        /*
+            Here we need to write the below code because
+            - the default destructor will not delete the pointer inside a pointer as
+            the all the nodes a dynamically create and each node has pointer inside a pointer.
+            - we  need to set the next to null other-wise the delete will recursively delete all the nodes.
+        */
+        if (this->next != NULL)
+        {
+            this->next = NULL;
+            delete next;
+        }
+    }
 };
 
 void insert_at_head(Node *&head, int data)
@@ -42,6 +57,7 @@ void insert_at_position(Node *&head, Node *&tail, int data, int index)
     for (int i = 0; i < index - 1; i++)
     {
         temp = temp->next;
+
         if (temp == NULL)
         {
             throw out_of_range("Index out of range");
@@ -58,14 +74,47 @@ void insert_at_position(Node *&head, Node *&tail, int data, int index)
     temp->next = node_to_insert;
 }
 
+void delete_at_position(Node *&head, Node *&tail, int index)
+{
+    Node *temp = head;
+
+    if (index == 0)
+    {
+        head = head->next;
+        delete temp;
+        return;
+    }
+
+    for (int i = 0; i < index - 1; i++)
+    {
+        temp = temp->next;
+        if (temp->next == NULL)
+        {
+            throw out_of_range("Index out of range");
+        }
+    }
+
+    Node *node_to_delete = temp->next;
+    temp->next = node_to_delete->next;
+
+    if (temp->next == NULL)
+    {
+        tail = temp;
+    }
+
+    delete node_to_delete;
+}
+
 void print(Node *&head)
 {
     Node *temp = head;
+
     while (temp != NULL)
     {
         cout << temp->data << " ";
         temp = temp->next;
     }
+
     cout << endl;
 }
 
@@ -86,6 +135,15 @@ int main()
     insert_at_head(head, 0);
 
     print(head);
+
+    delete_at_position(head, tail, 5);
+
+    print(head);
+
+    while (head != NULL)
+    {
+        delete_at_position(head, tail, 0);
+    }
 
     return 0;
 }
