317449e1f83c0bc837bf156543684cf5cb6503b4
Hwkas <amaansaifi999@gmail.com>
2023-08-06 17:29:27 +0530

    optimization

--- stack/stack_minimun_cost_to_make_string_valid.cpp
+++ stack/stack_minimun_cost_to_make_string_valid.cpp
@@ -7,7 +7,15 @@ int findMinimumCost(string str)
     // Write your code here
     stack<char> st;
 
-    for (size_t i = 0; i < str.size(); i++)
+    size_t size = str.size();
+
+    if (size % 2 != 0)
+    {
+        return -1;
+    }
+
+    // creating invalid strings stacks
+    for (size_t i = 0; i < size; i++)
     {
         if (str[i] == '{')
         {
@@ -34,15 +42,12 @@ int findMinimumCost(string str)
     int cost = 0;
     char ch;
 
+    // calculating cost
     while (!st.empty())
     {
         ch = st.top();
         st.pop();
-        if (st.empty())
-        {
-            return -1;
-        }
-        else if (ch == st.top())
+        if (ch == st.top())
         {
             cost++;
         }
