3c885c2ea7b61f60fa7043182ad8dd1cbd4ae445
Hwkas <amaansaifi999@gmail.com>
2023-08-06 17:59:58 +0530

    aother approch

--- stack/stack_minimun_cost_to_make_string_valid.cpp
+++ stack/stack_minimun_cost_to_make_string_valid.cpp
@@ -39,14 +39,16 @@ int findMinimumCost(string str)
         return 0;
     }
 
-    int cost = 0;
-    char ch;
-
     // calculating cost
+    int cost = 0;
+
+    // approch 1
+    char ch;
     while (!st.empty())
     {
         ch = st.top();
         st.pop();
+
         if (ch == st.top())
         {
             cost++;
@@ -55,8 +57,29 @@ int findMinimumCost(string str)
         {
             cost += 2;
         }
+
         st.pop();
     }
+
+    // approch 2
+    // int opening_braces_count = 0;
+    // int closing_brances_count = 0;
+
+    // while (!st.empty())
+    // {
+    //     if (st.top() == '{')
+    //     {
+    //         opening_braces_count++;
+    //     }
+    //     else
+    //     {
+    //         closing_brances_count++;
+    //     }
+
+    //     st.pop();
+    // }
+
+    // cost = (++opening_braces_count) / 2 + (++closing_brances_count) / 2;
 
     return cost;
 }
